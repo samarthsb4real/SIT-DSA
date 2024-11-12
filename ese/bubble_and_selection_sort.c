72c30db90197becf97ff684676ebd3a1ee353bb4
samarthsb4real <samarthbhadane23@gmail.com>
2024-11-13 01:59:04 +0530

    Bubble and Selection Sort Implementation

--- ese/bubble_and_selection_sort.c
+++ ese/bubble_and_selection_sort.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 // Bubble Sort
-void bubbleSort(int arr[], int n) {
+void bubbleSort(int arr[], int n)
+{
     for (int i = 0; i < n - 1; i++)
         for (int j = 0; j < n - i - 1; j++)
-            if (arr[j] > arr[j + 1]) {
+            if (arr[j] > arr[j + 1])
+            {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
@@ -12,8 +14,10 @@ void bubbleSort(int arr[], int n) {
 }
 
 // Selection Sort
-void selectionSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
         int min_idx = i;
         for (int j = i + 1; j < n; j++)
             if (arr[j] < arr[min_idx])
@@ -24,13 +28,15 @@ void selectionSort(int arr[], int n) {
     }
 }
 
-void printArray(int arr[], int size) {
+void printArray(int arr[], int size)
+{
     for (int i = 0; i < size; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
 
-int main() {
+int main()
+{
     int arr[] = {64, 25, 12, 22, 11};
     int n = sizeof(arr) / sizeof(arr[0]);
 
