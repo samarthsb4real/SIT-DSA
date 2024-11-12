644cd34d595786d9820d367bde2048e8d0d1f3ac
samarthsb4real <samarthbhadane23@gmail.com>
2024-11-13 01:51:43 +0530

    BST- to Insert, Search, Delete, Inorder Traversal

---
+++ ese/BST-isdi.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* insert(struct Node* root, int data) {
+    if (root == NULL) return createNode(data);
+    if (data < root->data) root->left = insert(root->left, data);
+    else if (data > root->data) root->right = insert(root->right, data);
+    return root;
+}
+
+struct Node* minValueNode(struct Node* node) {
+    struct Node* current = node;
+    while (current && current->left != NULL) current = current->left;
+    return current;
+}
+
+struct Node* deleteNode(struct Node* root, int data) {
+    if (root == NULL) return root;
+    if (data < root->data) root->left = deleteNode(root->left, data);
+    else if (data > root->data) root->right = deleteNode(root->right, data);
+    else {
+        if (root->left == NULL) return root->right;
+        else if (root->right == NULL) return root->left;
+        struct Node* temp = minValueNode(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
+void inorder(struct Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+int main() {
+    struct Node* root = NULL;
+    int choice, data;
+
+    while (1) {
+        printf("\n1. Insert\n2. Delete\n3. Inorder\n4. Exit\nChoice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter data: ");
+                scanf("%d", &data);
+                root = insert(root, data);
+                break;
+            case 2:
+                printf("Enter data to delete: ");
+                scanf("%d", &data);
+                root = deleteNode(root, data);
+                break;
+            case 3:
+                printf("Inorder: ");
+                inorder(root);
+                printf("\n");
+                break;
+            case 4: exit(0);
+            default: printf("Invalid choice\n");
+        }
+    }
+    return 0;
+}
